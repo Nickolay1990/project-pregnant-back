1a7e745d9d46ce44dccfa4475ce26392a0a94f50
Oleksandr Palilov <opalilovit@gmail.com>
2025-09-19 21:34:14 +0300

    added api users route

---
+++ src/controllers/users.js
@@ -0,0 +1,54 @@
+import createHttpError from 'http-errors';
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
+import { updateUser } from '../services/users.js';
+
+export const getCurrentUserController = async (req, res) => {
+  const user = req.user;
+  if (!user) {
+    throw createHttpError(404, 'User not found.');
+  }
+
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully found current user.',
+    data: user,
+  });
+};
+
+export const updateUserAvatarController = async (req, res, next) => {
+  const { userId } = req.params;
+
+  const photo = req.file;
+  let photoUrl;
+  if (photo) {
+    photoUrl = await saveFileToCloudinary(photo);
+  }
+
+  const result = await updateUser(userId, { ...req.body, photo: photoUrl });
+
+  if (!result) {
+    next(createHttpError(404, 'User not found.'));
+  }
+
+  res.json({
+    status: 200,
+    message: 'Successfully update avatar user!',
+    data: result,
+  });
+};
+
+export const updateUserController = async (req, res, next) => {
+  const { userId } = req.params;
+  const result = await updateUser(userId, req.body);
+
+  if (!result) {
+    next(createHttpError(404, 'User not found'));
+    return;
+  }
+
+  res.json({
+    status: 200,
+    message: `Successfully update a user!`,
+    data: result,
+  });
+};
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,29 @@
 import { Router } from 'express';
+import {
+  getCurrentUserController,
+  updateUserAvatarController,
+  updateUserController,
+} from '../controllers/users.js';
+import { isValidId } from '../middlewares/isValidId.js';
+import { upload } from '../middlewares/multer.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { updateUserSchema } from '../validation/auth.js';
 
-export const usersRouter = Router();
+const usersRouter = Router();
 
-// usersRouter.get('/:userId');
-// usersRouter.patch('/updateUserPhoto/:userId');
-// usersRouter.patch('/updateUserData/:userId');
+usersRouter.get('/currentUser', getCurrentUserController);
+usersRouter.patch(
+  '/updateUserPhoto/:userId',
+  isValidId,
+  validateBody(updateUserSchema),
+  upload.single('photo'),
+  updateUserAvatarController,
+);
+usersRouter.patch(
+  '/updateUserData/:userId',
+  isValidId,
+  validateBody(updateUserSchema),
+  updateUserController,
+);
+
+export default usersRouter;
